6898c47d40958a9e4141d4676edfe718cd3dd279
Pechnkv <m.pechnikov.95@gmail.com>
2024-11-17 14:48:06 +0900

    Add project files.

---
+++ Calc_ver.1/Calc_ver.1.cs
@@ -0,0 +1,51 @@
+﻿while (true)
+
+{
+
+    Console.Clear();
+
+
+    double a, b;
+    string action;
+
+    try
+    {
+        Console.WriteLine("Введите первое число");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второе число");
+        b = int.Parse(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Вы ввели не число");
+        Console.ReadLine();
+        continue;
+    }
+
+    Console.WriteLine("Выберите операцию");
+
+    action = Console.ReadLine();
+
+    switch (action)
+    {
+        case "+":
+            Console.WriteLine(a + b);
+            break;
+        case "-":
+            Console.WriteLine(a - b);
+            break;
+        case "/":
+            Console.WriteLine(a / b);
+            break;
+        case "*":
+            Console.WriteLine(a * b);
+            break;
+
+        default:
+            Console.WriteLine("Неизвестное действие");
+            break;
+    }
+    Console.ReadLine();
+
+}
+
---
+++ Calc_ver.2/Calc_ver.2.cs
@@ -0,0 +1,62 @@
+﻿while (true)
+{
+    Console.Clear();
+
+    string str1, str2;
+    int num1, num2;
+    string action;
+
+    for (; ; )
+    {
+       
+        Console.WriteLine("Введите первое число");
+        str1 = Console.ReadLine();
+        bool result1 = int.TryParse(str1, out num1);
+
+        if (result1)
+        {
+                       break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели буквы, введите заново");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+    }
+
+    for (; ; )
+    {
+
+        Console.WriteLine("Введите второе число");
+        str2 = Console.ReadLine();
+        bool result2 = int.TryParse(str2, out num2);
+
+        if (result2)
+        {
+           
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели буквы, введите заново");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+    }
+
+    Console.WriteLine("Выберите операцию. '+', '-', '*', '/'");
+    action = Console.ReadLine();
+    if (action == "+")
+        Console.WriteLine("Сумма равна = " + (num1 + num2));
+    else if (action == "-")
+        Console.WriteLine("Разница равна = " + (num1 - num2));
+    else if (action == "*")
+        Console.WriteLine("Умножение равно = " + (num1 * num2));
+    else if (action == "/")
+        Console.WriteLine("Деление равно = " + (num1 / num2));
+
+    Console.ReadLine();
+}
